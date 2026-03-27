db3405e198b2a9ad556a9b3a629985b4f6ef7d6f
Joanna Jacob <joannajacob910@gmail.com>
2026-03-27 14:36:51 +0530

    use case 5

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,13 @@ import java.util.*;
 
 class Main{
     public static void main(String[] args){
-        Set<String> bogieIds = new HashSet<>();
-        LinkedList<String> train = new LinkedList<>();
+        LinkedHashSet<String> train = new LinkedHashSet<>();
+
         train.add("Engine");
         train.add("Sleeper");
-        train.add("AC");
         train.add("Cargo");
         train.add("Guard");
-        System.out.println("Initial Train: " + train);
-        train.add(2, "Pantry Car");
-        System.out.println("After adding Pantry Car: " + train);
-        train.removeFirst();
-        train.removeLast();
-        System.out.println("Final Train Consist: " + train);
+        train.add("Sleeper");
+        System.out.println("Train Formation: " + train);
     }
 }

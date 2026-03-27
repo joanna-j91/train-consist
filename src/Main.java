9da0b584da026c9bcbb5f4ed5c87dca1f074bb80
Joanna Jacob <joannajacob910@gmail.com>
2026-03-27 14:09:35 +0530

    uce case 1 coplete

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Main{
+    public static void main(String[] args){
+        System.out.println("===============================");
+        System.out.println(" ===Train Consist Management=== ");
+        System.out.println("===============================");
+
+        System.out.println("Train initialized successfully...");
+
+        List<String> trainConsist = new ArrayList<>();
+
+        System.out.println("Initial Bogie Conut : " + trainConsist.size());
+
+        System.out.println("Current Train Consist: []");
+    }
+}

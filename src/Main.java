e2ef1d853d96e864e50f6a520f4f4d81791df36f
Joanna Jacob <joannajacob910@gmail.com>
2026-03-27 15:22:20 +0530

    use case 9 completed

--- src/Main.java
+++ src/Main.java
@@ -20,16 +20,19 @@ class Main{
         bogies.add(new Bogie("AC Chair", 56));
         bogies.add(new Bogie("First Class", 40));
         bogies.add(new Bogie("General", 90));
+        bogies.add(new Bogie("Sleeper", 72));
+        bogies.add(new Bogie("AC Chair", 56));
+
         System.out.println("\nAll Bogies:");
         for (Bogie b : bogies) {
             System.out.println(b);
         }
-        List<Bogie> filteredBogies = bogies
-                .stream()
-                .filter(b -> b.capacity > 60)
-                .collect(Collectors.toList());
-        System.out.println("\nFiltered Bogies (Capacity > 60):");
-        for (Bogie b : filteredBogies) {
-            System.out.println(b);
+        Map<String, List<Bogie>> groupedBogies = bogies.stream().collect(Collectors.groupingBy(b -> b.name));
+        System.out.println("\nGrouped Bogies By Type:");
+        for (Map.Entry<String, List<Bogie>> entry : groupedBogies.entrySet()) {
+            System.out.println("\nBogie Type: " + entry.getKey());
+            for (Bogie b : entry.getValue()) {
+                System.out.println("  " + b);
+            }
     }
 }

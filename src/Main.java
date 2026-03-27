7af2802ae7cc21ad5bf6980c739ad2fc2d50d413
Joanna Jacob <joannajacob910@gmail.com>
2026-03-27 14:39:29 +0530

    use case 6

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,12 @@ import java.util.*;
 
 class Main{
     public static void main(String[] args){
-        LinkedHashSet<String> train = new LinkedHashSet<>();
-
-        train.add("Engine");
-        train.add("Sleeper");
-        train.add("Cargo");
-        train.add("Guard");
-        train.add("Sleeper");
-        System.out.println("Train Formation: " + train);
+        HashMap<String, Integer> bogieCapacity = new HashMap<>();
+        bogieCapacity.put("Sleeper", 72);
+        bogieCapacity.put("AC Chair", 60);
+        bogieCapacity.put("First Class", 40);
+        for (Map.Entry<String, Integer> entry : bogieCapacity.entrySet()) {
+            System.out.println(entry.getKey() + " Capacity: " + entry.getValue());
+        }
     }
 }

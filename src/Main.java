9ec19651f37481859fd681827c3bf8206dcaf381
Joanna Jacob <joannajacob910@gmail.com>
2026-03-27 14:31:32 +0530

    use case 4 complete

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,17 @@ import java.util.*;
 class Main{
     public static void main(String[] args){
         Set<String> bogieIds = new HashSet<>();
-        bogieIds.add("BG101");
-        bogieIds.add("BG102");
-        bogieIds.add("BG103");
-        bogieIds.add("BG101");
-        bogieIds.add("BG102");
-        System.out.println("Unique Bogie IDs: " + bogieIds);
+        LinkedList<String> train = new LinkedList<>();
+        train.add("Engine");
+        train.add("Sleeper");
+        train.add("AC");
+        train.add("Cargo");
+        train.add("Guard");
+        System.out.println("Initial Train: " + train);
+        train.add(2, "Pantry Car");
+        System.out.println("After adding Pantry Car: " + train);
+        train.removeFirst();
+        train.removeLast();
+        System.out.println("Final Train Consist: " + train);
     }
 }

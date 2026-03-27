962d7d1e61b8df4961050cf7b6876c81d5f22a27
Joanna Jacob <joannajacob910@gmail.com>
2026-03-27 15:42:35 +0530

    use case 10 completed

--- src/Main.java
+++ src/Main.java
@@ -16,23 +16,20 @@ class Bogie {
 class Main{
     public static void main(String[] args){
         List<Bogie> bogies = new ArrayList<>();
-        bogies.add(new Bogie("Sleeper", 72));
-        bogies.add(new Bogie("AC Chair", 56));
+
         bogies.add(new Bogie("First Class", 40));
         bogies.add(new Bogie("General", 90));
         bogies.add(new Bogie("Sleeper", 72));
         bogies.add(new Bogie("AC Chair", 56));
 
-        System.out.println("\nAll Bogies:");
+        System.out.println("\nBogies in Train:");
         for (Bogie b : bogies) {
             System.out.println(b);
         }
-        Map<String, List<Bogie>> groupedBogies = bogies.stream().collect(Collectors.groupingBy(b -> b.name));
-        System.out.println("\nGrouped Bogies By Type:");
-        for (Map.Entry<String, List<Bogie>> entry : groupedBogies.entrySet()) {
-            System.out.println("\nBogie Type: " + entry.getKey());
-            for (Bogie b : entry.getValue()) {
-                System.out.println("  " + b);
-            }
+        int totalSeats = bogies.map(b -> b.capacity).reduce(0, Integer::sum);
+
+        System.out.println("\nTotal Seating Capacity of Train: " + totalSeats);
+
+        }
     }
 }
